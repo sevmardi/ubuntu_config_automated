4bc81cf363d5e91b79574d1a890d6aa30a695e9a
Sevak Mardirosian <smardirosians@gmail.com>
2018-12-02 20:29:54 +0100

    adding docker compose

--- script.sh
+++ script.sh
@@ -153,6 +153,8 @@ mv composer.phar /usr/local/bin/composer
 #Docker
 echo "################################ Installing Docker #########################################"
 apt install docker.io
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 #postgreSQL
 echo "################################ Installing postgreSQL #########################################"
